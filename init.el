be69561a270f37dff29deb59b5b8d8b50ebf3f78
Juha Jeronen <juha.jeronen@jyu.fi>
2019-05-20 16:07:23 +0300

    Set up flake8 config file location for python-mode

--- init.el
+++ init.el
@@ -793,6 +793,7 @@ This function is called at the very end of Spacemacs initialization."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(evil-want-Y-yank-to-eol nil)
+ '(flycheck-flake8rc "~/.config/flake8")
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-mypy-executable "python3")
  '(flycheck-python-pycompile-executable "python3")
