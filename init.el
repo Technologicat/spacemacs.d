65ac987d313b201aa16ab943f9f2e8bce5eebb7b
Juha Jeronen <juha.jeronen@jyu.fi>
2019-05-26 02:01:35 +0300

    add link to emacs package dev handbook, lots of useful tricks

--- init.el
+++ init.el
@@ -463,6 +463,9 @@ It should only modify the values of Spacemacs settings."
 ;; https://www.masteringemacs.org/article/searching-buffers-occur-mode
 ;; TODO: is this the right place for a compile-time requirement in Spacemacs?
 
+;; with-current-buffer is slow; use buffer-local-value instead; see:
+;; https://alphapapa.github.io/emacs-package-dev-handbook/
+;;
 ;; helm-swoop.el already defines this and automatically invokes buffer-name on each result.
 ;; (defun get-buffers-matching-mode (mode)
 ;;   "Returns a list of buffers where their major-mode is equal to MODE"
