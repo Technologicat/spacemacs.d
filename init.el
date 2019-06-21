b80fedef56880d9c35cd4eb31d7224ac61adf0a1
Juha Jeronen <juha.jeronen@jyu.fi>
2019-06-21 23:52:29 +0300

    add protect keyword (nonlocal exits), seems to be required to get the advice to actually fire

--- init.el
+++ init.el
@@ -875,7 +875,7 @@ If SPLIT-ONEWINDOW is non-`nil' window is split in persistent action."
   ;; https://github.com/zk-phi/phi-search/issues/53
   (require 'mc-extras)
   (with-eval-after-load "phi-search"
-    (defadvice phi-search-complete (after phi-search-auto-dedup last activate)
+    (defadvice phi-search-complete (after phi-search-auto-dedup last activate protect)
       (mc/remove-duplicated-cursors)))
   ;; https://www.reddit.com/r/emacs/comments/90si1c/why_cant_i_have_truly_persistent_undo_on_emacs/
   ;; default undo-limit is 80k, which is way too small in a large file if I accidentally `C-x C-p RET'
