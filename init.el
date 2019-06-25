8d2662c2435d7a96f68a4cbdc44b390ae6ba568a
Juha Jeronen <juha.jeronen@jyu.fi>
2019-06-25 21:28:32 +0300

    remove unneeded autogenerated default face setting

--- init.el
+++ init.el
@@ -1128,7 +1128,6 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
  '(ahs-face ((t (:background "olive drab" :foreground "GhostWhite"))))
  '(ahs-plugin-whole-buffer-face ((t (:background "dark olive green" :foreground "ghost white"))))
  '(bmkp-light-autonamed ((t (:background "SteelBlue4"))))
