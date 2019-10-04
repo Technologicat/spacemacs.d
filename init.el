d18ee0bbf4f79205c201810c98f7190d2de0399c
Juha Jeronen <juha.jeronen@gbw.fi>
2019-10-04 12:19:32 +0300

    Add octave layer for reading GNU Octave and MATLAB code

--- init.el
+++ init.el
@@ -44,7 +44,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(javascript
+   '(octave
+     javascript
      html
      csv
      yaml
