dfcb7236fba3b78e036f7b6efd113928dffb59ca
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-06-10 09:59:22 +0300

    prettify-symbols: Julia: prettify the singleton type Nothing

--- prettify-symbols-config.el
+++ prettify-symbols-config.el
@@ -152,6 +152,7 @@
                  ("Complex" . ?ℂ)
                  ("Bool" . (?ℤ (Br . Bl) ?₂))  ; ℤ₂ ...maybe bad taste?
                  ("Function" . ?λ)
+                 ("Nothing" . ?∅)  ; singleton type containing the value `nothing'
                  ("Any" . ?⊤)  ; top type, down tack symbol
                  ; in Julia, the bottom type is `Union{}', which is not a lone symbol, so we can't convert that.
                  ;; Greek symbols for mathematical usage
