2ebc2acd62d43cefd6d003c3f8614e9fc9d55d37
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-08-18 02:01:46 +0300

    update unpythonic syntax highlight

--- prettify-symbols-config.el
+++ prettify-symbols-config.el
@@ -205,7 +205,7 @@
   (defun my/unpythonic-syntax-highlight-setup ()
     "Set up additional syntax highlighting for `unpythonic.syntax' and `macropy3' in python mode."
     ;; adapted from code in dash.el
-    (let ((new-keywords '("test" "test_raises" "test_signals"
+    (let ((new-keywords '("test" "test_raises" "test_signals" "fail" "error"
                           "signal" "handlers" "restarts" ; not macros, but in a role similar to exception handling constructs in the conditions/restarts system.
                           "let" "dlet" "blet"
                           "letseq" "dletseq" "bletseq"
