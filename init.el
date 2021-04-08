81cdf79fe916fd5b20c621a8e6f21ba12444645a
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-04-08 12:07:03 +0300

    add "rust" layer

--- init.el
+++ init.el
@@ -44,7 +44,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(octave
+   '(rust
+     octave
      javascript
      html
      csv
